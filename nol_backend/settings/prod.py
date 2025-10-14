25902a309e656cfa75fea14b0e9d6bb36b703044
tushar-3549 <tushardiu3549@gmail.com>
2025-10-14 11:44:04 -0700

    db deployed

--- nol_backend/settings/prod.py
+++ nol_backend/settings/prod.py
@@ -14,6 +14,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
+        
+        'OPTIONS': { 'sslmode': 'require' },  
+        'CONN_MAX_AGE': 60,
     }
 }
 
